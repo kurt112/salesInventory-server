a869ee8ffd08e10cff4012ac363315a926a48b1d
Kurt <kurtorioque112@gmail.com>
2021-04-18 06:33:19 +0800

    Supplier Update Added

--- routes/SupplierRoute.js
+++ routes/SupplierRoute.js
@@ -6,7 +6,7 @@ router.post('/insert', async (req, res) => {
 
     await Supplier.create(req.body).then(e => {
         res.send(e)
-    }).catch(e => {
+    }).catch(ignored => {
         res.status(400).send({
             title: 'Email Should be unique',
             message: 'supplier email is existing'
@@ -26,6 +26,7 @@ router.get('/list', (req, res) => {
 
 
 router.post('/update', async (req, res) => {
+    console.log(req.body.email)
     try {
         const result = await Supplier.update(req.body,
             {
@@ -47,16 +48,49 @@ router.post('/update', async (req, res) => {
 
 router.post('/delete', async (req, res) => {
 
-    await Supplier.destroy({}).then(e => {
-        res.send(e)
-    }).catch(ignored => {
-        res.status(400).send({
-            title: `Supplier can't find`,
-            message: `Can't update Supplier`
+    const email = req.body.email
+
+
+    let error;
+    try {
+        const supplier = await Supplier.destroy({
+            where: {email}
         })
-    })
+
+        if (supplier === 0) throw new Error('No Supplier')
+    } catch (e) {
+        error = e
+    }
+
+    if (error) {
+        res.status(400).send({
+            title: `Supplier Delete`,
+            message: `Deleting this supplier can cause fatal error`
+        })
+    } else res.send("Supplier Deleted Success")
+
 
 })
 
+router.post('/find', async (req,res) => {
+
+    const {email} = req.body
+
+    const data = await Supplier.findAll({
+        limit: 1,
+        where: {
+            email
+        }
+    })
+
+    if(data.length === 0){
+        res.status(400).send({
+            title: 'Supplier Not Found',
+            message: 'Enter Proper Supplier Email'
+        })
+    }else{
+        res.send(data)
+    }
+})
 
 module.exports = router
