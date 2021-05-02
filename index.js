cdd2480228adefe0f3f5e1166536c4574a87bc1b
Kurt <kurtorioque112@gmail.com>
2021-05-02 14:49:06 +0800

    adfasdf

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express")
-// const db = require('./models')
-// const cors = require('cors')
-// const bodyParser = require('body-parser');
-// const fileUpload = require('express-fileupload')
+const db = require('./models')
+const cors = require('cors')
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload')
 const app = express()
-// const verify = require('./utils/jwt')
-// const {User, Store,Customer,Setting} = require('./models')
-// // setting up cors
+const verify = require('./utils/jwt')
+const {User, Store,Customer,Setting} = require('./models')
+// setting up cors
 // app.use(cors(
 //     {
 //         methods: ['GET', 'POST', 'PATCH', 'DELETE'],
@@ -14,34 +14,34 @@ const app = express()
 //     }
 // ))
 //
-// app.use(bodyParser.json());
-// app.use(fileUpload());
-//
-// // Routes For Server
-// const ProductRoute = require("./routes/ProductRoute")
-// const CustomerRoute = require("./routes/CustomerRoute")
-// const SalesRoute = require("./routes/SalesRoute")
-// const StoreRoute = require("./routes/StoreRoute")
-// const SupplierRoute = require("./routes/SupplierRoute")
-// const TransactionRoute = require("./routes/TransactionRoute")
-// const UserRoute = require("./routes/UserRoute")
-// const AuditTrailRoute = require('./routes/AuditTrail')
-// const DashBoardRoute = require('./routes/DashBoardRoute')
-// const Auth = require('./routes/Authentication')
-// const Settings = require('./routes/SettingsRoute')
-//
-// // route implementation
-// app.use('/product', ProductRoute)
-// app.use('/user', verify, UserRoute)
-// app.use('/transaction', verify, TransactionRoute)
-// app.use('/supplier', verify, SupplierRoute)
-// app.use('/store', verify, StoreRoute)
-// app.use('/sales', verify, SalesRoute)
-// app.use('/customer', verify, CustomerRoute)
-// app.use('/audit', verify, AuditTrailRoute)
-// app.use('/dashboard', verify, DashBoardRoute)
-// app.use('/setting', verify, Settings)
-// app.use('/', Auth)
+app.use(bodyParser.json());
+app.use(fileUpload());
+
+// Routes For Server
+const ProductRoute = require("./routes/ProductRoute")
+const CustomerRoute = require("./routes/CustomerRoute")
+const SalesRoute = require("./routes/SalesRoute")
+const StoreRoute = require("./routes/StoreRoute")
+const SupplierRoute = require("./routes/SupplierRoute")
+const TransactionRoute = require("./routes/TransactionRoute")
+const UserRoute = require("./routes/UserRoute")
+const AuditTrailRoute = require('./routes/AuditTrail')
+const DashBoardRoute = require('./routes/DashBoardRoute')
+const Auth = require('./routes/Authentication')
+const Settings = require('./routes/SettingsRoute')
+
+// route implementation
+app.use('/product', ProductRoute)
+app.use('/user', verify, UserRoute)
+app.use('/transaction', verify, TransactionRoute)
+app.use('/supplier', verify, SupplierRoute)
+app.use('/store', verify, StoreRoute)
+app.use('/sales', verify, SalesRoute)
+app.use('/customer', verify, CustomerRoute)
+app.use('/audit', verify, AuditTrailRoute)
+app.use('/dashboard', verify, DashBoardRoute)
+app.use('/setting', verify, Settings)
+app.use('/', Auth)
 //
 // app.post('/upload', async (req, res) => {
 //
